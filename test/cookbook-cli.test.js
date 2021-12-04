8a21dd4ca400aeda2632caa0752b627d797603b9
Romualdo <132roldan@gmail.com>
2021-12-04 06:37:47 -0300

    cli tests passed

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -4,18 +4,39 @@ const { CookbookCli } = require('../src/cookbook-cli');
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
-      
+      const myCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myCookbook);
+
+      const message = myCookbookCli.add('hotdog', ['meat', 'bread']);
+
+      expect(message).toEqual('Successfully added the following recipe: hotdog')
     });
   });
 
   describe('Listing recipes', () => {
     test('should display the correct message listing all of the recipe names', () => {
-      
+      const myCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myCookbook);
+
+      myCookbookCli.add('hotdog', ['meat', 'bread'])
+
+      const message = myCookbookCli.list();
+
+      expect(message).toEqual('You have the following recipes: hotdog')
+
+     
     });
   });
 
   describe('Retrieving a recipe', () => {
     test('should display the ingredients required to make the specified recipe', () => {
+      const myCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myCookbook);
+
+      myCookbookCli.add('hotdog', ['meat', 'bread'])
+      const message = myCookbookCli.get('hotdog')
+      console.log(message);
+      expect(message).toEqual('The ingredients for hotdog are: meat,bread')
       
     });
   });
